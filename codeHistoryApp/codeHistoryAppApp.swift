f45188096fef9d7c988aee5cb55aa5d5092f9cf5
Rafael Ribeiro <rafaelribeiro2004@icloud.com>
2025-05-18 16:19:53 -0300

    Initial Commit

---
+++ codeHistoryApp/codeHistoryAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeHistoryAppApp.swift
+//  codeHistoryApp
+//
+//  Created by Rafael Ribeiro on 5/18/25.
+//
+
+import SwiftUI
+
+@main
+struct codeHistoryAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
