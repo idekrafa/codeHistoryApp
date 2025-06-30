701873c77320891981bea2870690b58473cbbaf2
Rafael Ribeiro <rafaelribeiro2004@icloud.com>
2025-06-30 18:04:47 -0300

    Project is DONE! :D
    The entire Codehistory app now works as expected! You can answer the questions and get the score at the end

---
+++ Views/ScoreView.swift
@@ -0,0 +1,18 @@
+//
+//  ScoreView.swift
+//  codeHistoryApp
+//
+//  Created by Rafael Ribeiro on 6/30/25.
+//
+
+import SwiftUI
+
+struct ScoreView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ScoreView()
+}
---
+++ codeHistoryApp/Models/ScoreViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ScoreViewModel.swift
+//  codeHistoryApp
+//
+//  Created by Rafael Ribeiro on 6/30/25.
+//
+
+import Foundation
